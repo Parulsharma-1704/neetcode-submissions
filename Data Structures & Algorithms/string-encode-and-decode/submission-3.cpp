b6cb6535e160bf4ebd81f3845761ed42b248cfd9
Parul Sharma <149969583+Parulsharma-1704@users.noreply.github.com>
2026-09-01 17:14:36 +0530

    Add: string-encode-and-decode - submission-3

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-3.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        if(strs.empty()){
+            return "";
+        }
+        string temp=strs[0];
+        for(int i=1;i<strs.size();i++){
+            temp+="^" + strs[i];
+        }
+        return temp;
+    }
+
+    vector<string> decode(string s) {
+        vector<string>ans;
+        if(s.empty()){
+            return {""};
+        }
+        string temp="";
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='^'){
+                ans.push_back(temp);
+                temp="";
+                continue;
+            }
+            temp+=s[i];
+        }
+        ans.push_back(temp);
+        // ans.push_back(s);
+        return ans;
+    }
+};
